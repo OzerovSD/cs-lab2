dc62a6bca3dbcbf9b796eb97f9ce061a676b1bf4
Alice (OzerovSD) <serejaozeroff@yandex.ru>
2022-03-20 20:31:00 +0300

    code: 2-й вариант вывода суммы и разности

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
+    cout << "Sum: " << a + b << endl;
+    cout << "Raz: " << a - b << endl;
     cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n';
+         << "A - B = " << a - b << '\n';
 }
