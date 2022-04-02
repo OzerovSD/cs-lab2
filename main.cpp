7ede440b29e6cbf758724855f6b96471b80c725e
Alice (OzerovSD) <serejaozeroff@yandex.ru>
2022-04-02 19:03:43 +0300

    build: изменение конфигурации

--- main.cpp
+++ main.cpp
@@ -7,23 +7,16 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-<<<<<<< HEAD
-    if (a>b)
-=======
+
         if (a>b)
->>>>>>> code: вывод максимума
-        cout <<"Max = " << a << '\n';
+{
+    cout <<"Max = " << a << '\n';
+}
     else
-        cout <<"Max = " << b<< '\n';
+ {
+    cout <<"Max = " << b<< '\n';
+ }
 
-<<<<<<< HEAD
-=======
-        if (a<b)
-        cout <<"Min = " << a << '\n';
-    else
-        cout <<"Min = " << b << '\n';
-
->>>>>>> code: вывод минимума
     cout << "Sum: " << a + b << endl;
     cout << "Raz: " << a - b << endl;
     cout << "Proiz: " <<a*b<< endl;
